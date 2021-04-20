b90a48b26f0a9e81e7ca61843a1937e6230ea926
Lucas Santos de Souza <lucas.santos@zoom.com.br>
2021-04-20 00:34:35 -0300

    testing precommit hook

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+import { Button } from "./styles";
+{
+}
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Teste</div>
+      <Button>Teste</Button>
+    </div>
+  );
+};
+
+export default Home;
