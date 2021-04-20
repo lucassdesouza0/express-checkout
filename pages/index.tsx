20bf5df9d51a4fa9921680725dc72382ec5849fc
Lucas Santos de Souza <lucas.santos@zoom.com.br>
2021-04-20 11:53:56 -0300

    testing precommit hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ const Home: React.FC = () => {
     <div>
       <Head>
         <title>Create Next App s</title>
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>Teste</div>
